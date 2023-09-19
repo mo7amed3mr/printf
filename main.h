9ba73423df8e8c463175bd0d1623d3904d30000f
Adamb7r <bahrcr7@gmail.com>
2023-09-19 09:37:47 -0700

    fixed main.h

--- main.h
+++ main.h
@@ -3,39 +3,8 @@
 
 #include <stdarg.h>
 #include <stdio.h>
-
+#include <string.h>
 #include <unistd.h>
-#define UNUSED(x) (void)(x)
-
-#define BUFF_SIZE 1024
-#define F_MINUS 1
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 16
-#define S_LONG 2
-#define S_SHORT 1
-
-/**
- * struct fmt - Struct op
- *
- * @fmt: The format.
- * @fn: The function associated.
- */
-struct fmt
-{
-	char fmt;
-	int (*fn)(va_list, char[], int, int, int, int);
-};
-
-
-/**
- * typedef struct fmt fmt_t - Struct op
- *
- * @fmt: The format.
- * @fm_t: The function associated.
- */
-typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 
