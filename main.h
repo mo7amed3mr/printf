36f115d8879f7d2b8e81ee431aa17fdffb7a7610
Adamb7r <bahrcr7@gmail.com>
2023-09-19 09:35:30 -0700

    fixed main.h

--- main.h
+++ main.h
@@ -38,19 +38,5 @@ struct fmt
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i,
-va_list list, char buffer[], int flags, int width, int precision, int size);
-
-/* print chars and strings */
-int print_char(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_string(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_percent(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-
-/* print numbers */
-int print_int(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
 
 #endif
